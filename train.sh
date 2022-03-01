3b867c9c3beaba0d7819853f28bc88323fa35928
zengwang <zengwang430521@gmail.com>
2022-03-01 21:12:18 +0800

    new train

---
+++ train.sh
@@ -0,0 +1,10 @@
+srun -p mm_human --quotatype=auto\
+srun -p pat_earth -x SH-IDC1-10-198-4-[100-103,116-119] \
+srun -p pat_earth \
+    --ntasks=8 --gres=gpu:8 --ntasks-per-node=8 --cpus-per-task=5 --kill-on-bad-exit=1 \
+    --job-name=dvt \
+    python -u main.py /mnt/lustre/zengwang/data/imagenet \
+    --model DVT_T2t_vit_12 --batch-size 128 --lr 2e-3 --weight-decay .03 --amp --img-size 224
+
+    python -m torch.distributed.launch --nproc_per_node=8 main.py /mnt/lustre/zengwang/data/imagenet \
+    --model DVT_T2t_vit_12 --batch-size 128 --lr 2e-3 --weight-decay .03 --amp --img-size 224
